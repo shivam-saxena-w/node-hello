b0d124a3e2e7cbb9a6d81b87eb75ee92b5098810
Shivam Saxena <shivam.saxena@knoldus.com>
2021-12-17 16:25:13 +0530

    added poll scm check

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\nhello'
+  const msg = 'Hello Node!\nhello\nHello Node!\nhelloHello Node!\nhello'
   res.end(msg);
 });
 
